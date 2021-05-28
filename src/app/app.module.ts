e7668045a504dd65e975ea762367ae49bae3c41b
Kaartikeyan Raghavan <kaarthik.raghavan@gmail.com>
2021-05-28 23:56:17 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './site/header/header.component';
+import { FooterComponent } from './site/footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { TweetComponent } from './tweet/tweet.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    TweetComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
